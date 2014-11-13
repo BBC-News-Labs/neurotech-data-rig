3dec7a2fef5059dd83d971624647da5b10c86805
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-11-13 17:39:05 +0000

    serve webapp through nginx

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,3 +40,37 @@ service { "deja":
   ensure   => "running",
   provider => "upstart", 
 }
+
+package { "nginx":
+  ensure => "installed",
+}
+
+file { "/etc/nginx/sites-available/deja":
+  ensure => "file",
+  source => "/tmp/puppet-files/deja_nginx.conf",
+  owner => "root",
+  group => "root",
+  mode => "644",
+  require => Package["nginx"],
+  notify => Service["nginx"],
+}
+
+file { "/etc/nginx/sites-enabled/default":
+  ensure => "absent",
+  require => Package["nginx"],
+  notify => Service["nginx"],
+}
+
+file { "/etc/nginx/sites-enabled/deja":
+  ensure => "link",
+  target => "/etc/nginx/sites-available/deja",
+  require => File["/etc/nginx/sites-available/deja"],
+  notify => Service["nginx"],
+}
+
+service { "nginx":
+  ensure  => "running",
+  enable  => "true",
+  require => Package["nginx"],
+}
+
