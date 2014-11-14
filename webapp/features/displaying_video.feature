b5de8a651daad63d8aea7b52ae938fabaa5d6f2b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-11-14 15:46:48 +0000

    failing feature for next video buttons

--- webapp/features/displaying_video.feature
+++ webapp/features/displaying_video.feature
@@ -6,3 +6,13 @@ Feature: Displaying video to test subjects
     When I view the web application
     Then I should see a video player with a news archive clip
     And I should be prompted to play the video
+
+  Scenario: A test subject chooses a similar video
+    Given I have watched a video
+    When I indicate that I remember the topic
+    Then I should be shown another video on the same topic
+
+  Scenario: A test subject chooses a different video
+    Given I have watched a video
+    When I indicate that I do not remember the topic
+    Then I should be shown a video on a different topic
--- webapp/features/step_definitions/displaying_video_steps.rb
+++ webapp/features/step_definitions/displaying_video_steps.rb
@@ -1,5 +1,28 @@
+Given(/^I have watched a video$/) do
+  visit "/"
+  @watched_video_url = page.find("#player source")["src"]
+  execute_script %{
+    HTMLElement.prototype["play"] = function() {};
+  }
+  click_button "Play"
+  execute_script %{
+    var player = document.getElementById("player");
+    var event = document.createEvent("CustomEvent");
+    event.initCustomEvent("ended", false, false, null);
+    player.dispatchEvent(event);
+  }
+end
+
 When(/^I view the web application$/) do
   visit "/"
+end
+
+When(/^I indicate that I remember the topic$/) do
+  click_button "I remember this"
+end
+
+When(/^I indicate that I do not remember the topic$/) do
+  click_button "I don't remember this"
 end
 
 Then(/^I should see a video player with a news archive clip$/) do
@@ -8,4 +31,14 @@ end
 
 Then(/^I should be prompted to play the video$/) do
   expect(page).to have_button("Play")
+end
+
+Then(/^I should be shown another video on the same topic$/) do
+  new_video_url = page.find("#player source")["src"]
+  expect(new_video_url.split("/")[2]).to eq(@watched_video_url.split("/")[2])
+end
+
+Then(/^I should be shown a video on a different topic$/) do
+  new_video_url = page.find("#player source")["src"]
+  expect(new_video_url.split("/")[2]).not_to eq(@watched_video_url.split("/")[2])
 end
